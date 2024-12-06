b09e71c170c75232baa9a6bf12c4df3b3f16bd45
Paul Barahona Figueroa <paulalexisbarahonafigueroa@gmail.com>
2024-12-05 22:32:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,20 +7,38 @@ const twoDialog = document.querySelector("#two-dialog");
 const threebButton = document.querySelector("#three-button");
 const threeDialog = document.querySelector("#three-dialog");
 
-if (oneDialog && onebButton) {
-  onebButton.addEventListener("click", () => {
-    oneDialog.showModal();
-  });
+function handleOpen(button, dialog) {
+  if (button && dialog) {
+    button.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  }
 }
 
-if (twoDialog && twobButton) {
-  twobButton.addEventListener("click", () => {
-    twoDialog.showModal();
-  });
+function handleClose(dialog) {
+  if (dialog) {
+    const iframe = dialog.querySelector("iframe");
+    const closeButton = dialog.querySelector("button");
+
+    if (closeButton && iframe) {
+      closeButton.addEventListener("click", () => {
+        dialog.close();
+      });
+
+      dialog.addEventListener("close", () => {
+        const originalSrc = iframe.src;
+        iframe.src = "";
+        iframe.src = originalSrc;
+      });
+    }
+  }
 }
 
-if (threeDialog && threebButton) {
-  threebButton.addEventListener("click", () => {
-    threeDialog.showModal();
-  });
-}
+handleOpen(onebButton, oneDialog);
+handleClose(oneDialog);
+
+handleOpen(twobButton, twoDialog);
+handleClose(twoDialog);
+
+handleOpen(threebButton, threeDialog);
+handleClose(threeDialog);
